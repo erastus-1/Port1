2b5adf806acad1d8ac2ddc18d25c5f7dae9804f7
erastus <erastuskariuki15@gmail.com>
2021-02-14 18:35:36 +0300

    add

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,26 @@
 $(document).ready(function () {
     $("#back").click(function () {
        $("#back").fadeToggle(1000);
-       $("#back-p").fadeToggle(1000);
+       $("#back-ul").fadeToggle(1000);
     });
-    $("#back-p").click(function () {
-       $("#back-p").fadeToggle(1000);
+    $("#back-ul").click(function () {
+       $("#back-ul").fadeToggle(1000);
        $("#back").fadeToggle(1000);
     });
     $("#front").click(function () {
        $("#front").fadeToggle(1000);
-       $("#front-p").fadeToggle(1000);
+       $("#front-ul").fadeToggle(1000);
     });
-    $("#front-p").click(function () {
-       $("#front-p").fadeToggle(1000);
+    $("#front-ul").click(function () {
+       $("#front-ul").fadeToggle(1000);
        $("#front").fadeToggle(1000);
     });
     $("#tools").click(function () {
        $("#tools").fadeToggle(1000);
-       $("#tools-p").fadeToggle(1000);
+       $("#tools-ul").fadeToggle(1000);
     });
-    $("#tools-p").click(function () {
-       $("#tools-p").fadeToggle(1000);
+    $("#tools-ul").click(function () {
+       $("#tools-ul").fadeToggle(1000);
        $("#tools").fadeToggle(1000);
     });
  
